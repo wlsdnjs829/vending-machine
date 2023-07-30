841eefbee2ebf925c4f55f23a28c488b7bdfe582
ljw0829 <olo_jinwon@naver.com>
2023-07-30 20:46:16 +0900

    - feat: git init

---
+++ src/main/kotlin/com/example/vendingmachine/VendingMachineApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vendingmachine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VendingMachineApplication
+
+fun main(args: Array<String>) {
+    runApplication<VendingMachineApplication>(*args)
+}
